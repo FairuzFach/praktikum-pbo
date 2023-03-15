80d9b5e78c0327396b92bb613373f7488fae1fd7
FairuzFach <100415324+FairuzFach@users.noreply.github.com>
2023-03-15 15:07:26 +0700

    Add files via upload

---
+++ Pertemuan 3/poligon/Poligon.java
@@ -0,0 +1,15 @@
+/*
+* File			: Poligon.java 
+* Penulis 		: Fairuz Fachrizal Adyatma
+* Deskripsi 		: Representasi dasar dari objek poligon (segi banyak)
+*/
+
+package org.poligon;
+
+public class Poligon{
+   protected int jumlahSisi;
+
+   public int getJumlahSisi(){
+      return this.jumlahSisi;
+   }
+}
